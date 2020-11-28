fd37b15ace5b79183652d37801240ecdb9dc7ca8
samantha-olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2020-11-28 11:03:32 -0500

    hover glow effect

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
-function changeMargin() {
-  var w = window.innerWidth
-  || document.documentElement.clientWidth
-  || document.body.clientWidth;
-
-  var h = window.innerHeight
-  || document.documentElement.clientHeight
-  || document.body.clientHeight;
-
-  let hdiv2 = - ( h / 2);
-  let wdiv2 = - ( w / 2);
-
-  document.getElementById("illustration").style.marginTop = hdiv2+"px";
-  document.getElementById("illustration").style.marginLeft = wdiv2+"px";
-}
-
-function checktoChange() {
-  if (document.getElementById("illustration").style.width > "500px") {
-    document.getElementById("illustration").style.marginTop = "0px";
-    document.getElementById("illustration").style.marginLeft = "0px";
-  } else {
-    changeMargin();
-  }
-}
-
-checktoChange();
-window.onresize = checktoChange;
-
 $(document).ready(function(){
 
+  function changeMargin() {
+    var w = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    var h = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+    let hpos = - ( h / 4);
+    let wpos = - ( w / 4);
+
+    document.getElementById("main").style.marginTop = hpos+"px";
+    document.getElementById("main").style.marginLeft = wpos+"px";
+
+  };
 
 
-var a = document.getElementById("theobject");
+  function checktoChange() {
+    if (document.getElementById("main").style.width > "500px") {
+      document.getElementById("main").style.marginTop = "0px";
+      document.getElementById("main").style.marginLeft = "0px";
+    } else {
+      changeMargin();
+    }
+  };
 
-            // It's important to add an load event listener to the object,
-            // as it will load the svg doc asynchronously
-            a.addEventListener("load",function(){
+  checktoChange();
+  window.onresize = checktoChange;
 
-                // get the inner DOM of the svg
-                var svgDoc = a.contentDocument;
-                // get the inner element by id
-                var element = svgDoc.getElementById("desktop");
-                // add behaviour
-                element.addEventListener("mouseenter", reveal, false);
-                element.addEventListener("mouseleave", hide, false);
-            }, false);
+  var svg = document.getElementById("svg");
+  var myList = ["#desktop","#laptop","#dog","#owl","#city","#poster","#lanyard"]
+  // It's important to add an load event listener to the object,
+  // as it will load the svg doc asynchronously
+  svg.addEventListener("load",function(){
+    // get the inner element by id
+    myList.forEach(function(item) {
+      var itembx = item + "-whitebox"
+      var itemblur = item + "-blur"
+
+      var timer;
+
+      $(item).hover(function () {
+          $(itembx).show();
+          $(itemblur).show();
+      });
+
+      $(item + "," + itembx).hover(function (e) {
+          if (timer) timer = clearTimeout(timer)
+      }).mouseleave(function (e) {
+          timer = setTimeout(function () {
+              $(itembx).hide();
+              $(itemblur).hide();
+          }, 1)
+      });
+    });
+  }, false);
+
+
+
 
 
 
 });
-
-
-function reveal(evt) {
-  theId = evt.currentTarget.id
-  comb = theId + "-whitebox"
-  document.getElementById(comb).style.display = "block";
-}
-function hide(evt) {
-  theId = evt.currentTarget.id
-  comb = theId + "-whitebox"
-  document.getElementById(comb).style.display = "none";
-}
